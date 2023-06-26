66135a5b82cf09bd5459d8e0f0aa567e9d4069d3
Ogo-Ayorinde Shekinah <ogoayorindeshekinah@gmail.com>
2023-06-26 21:07:05 +0100

    New images added to pages

---
+++ js/workscarousel.js
@@ -0,0 +1,53 @@
+const carouselItems = document.querySelectorAll(".ws-journal-item");
+const prevButton = document.querySelector(".carousel-prev");
+const nextButton = document.querySelector(".carousel-next");
+console.log(carouselItems.length);
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = carouselItems.length - 1;
+
+carouselItems.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+function showPrevSlide() {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  //   move slide by 100%
+  carouselItems.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+  updateButtonVisibility();
+}
+
+function showNextSlide() {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  carouselItems.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide - 1)}%)`;
+  });
+  updateButtonVisibility();
+}
+
+function updateButtonVisibility() {
+  if (curSlide === 0) {
+    prevButton.style.display = "none";
+    nextButton.style.display = "block";
+  } else if (curSlide === carouselItems.length - 1) {
+    prevButton.style.display = "block";
+    nextButton.style.display = "none";
+  } else {
+    prevButton.style.display = "block";
+    nextButton.style.display = "block";
+  }
+}
